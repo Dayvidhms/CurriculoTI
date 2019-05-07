6fe9e92a571973729b3feb494e26da28ec3f73c0
Dayvidson <dayvidhms@gmail.com>
2019-05-06 22:06:16 -0300

    Login

--- app/src/main/java/studio/dayvidson/curriculoti/view/MainActivity.java
+++ app/src/main/java/studio/dayvidson/curriculoti/view/MainActivity.java
@@ -1,27 +1,40 @@
 package studio.dayvidson.curriculoti.view;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import studio.dayvidson.curriculoti.R;
+import studio.dayvidson.curriculoti.ws.UsuarioWS;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btnEntrar;
     private Button btnCadastrar;
 
+    private EditText edtUser;
+    private EditText edtSenha;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnEntrar.setEnabled(false);
-
         btnEntrar = findViewById(R.id.btnLoginEntrar);
         btnCadastrar = findViewById(R.id.btnLoginCadastrar);
+
+        btnEntrar.setEnabled(false);
+        btnEntrar.setBackgroundResource(R.drawable.btn_shape_disabled);
+
+        edtUser = findViewById(R.id.edtLoginUser);
+        edtSenha = findViewById(R.id.edtLoginSenha);
 
         btnEntrar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,6 +42,40 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        TextWatcher textWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String user = edtUser.getText().toString();
+                String senha = edtSenha.getText().toString();
+
+                if(user.isEmpty()||senha.isEmpty()){
+                    btnEntrar.setBackgroundResource(R.drawable.btn_shape_disabled);
+                    btnEntrar.setEnabled(false);
+                }else{
+                    btnEntrar.setBackgroundResource(R.drawable.btn_shape);
+                    btnEntrar.setEnabled(true);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) { }
+        };
+
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                UsuarioWS ws = new UsuarioWS();
+                String a = ws.selectLogin(edtUser.getText().toString(), edtSenha.getText().toString());
+                Log.e("RETORNO DO SELECT TAOK", a);
+            }
+        });
+
+        edtSenha.addTextChangedListener(textWatcher);
+        edtUser.addTextChangedListener(textWatcher);
 
         btnCadastrar.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/studio/dayvidson/curriculoti/ws/UsuarioWS.java
+++ app/src/main/java/studio/dayvidson/curriculoti/ws/UsuarioWS.java
@@ -3,15 +3,20 @@ package studio.dayvidson.curriculoti.ws;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.os.StrictMode;
 
 import org.json.JSONObject;
 
+import java.io.IOException;
 import java.util.logging.Handler;
 import java.util.logging.LogRecord;
 
 import cz.msebera.android.httpclient.client.HttpClient;
+import cz.msebera.android.httpclient.client.ResponseHandler;
+import cz.msebera.android.httpclient.client.methods.HttpGet;
 import cz.msebera.android.httpclient.client.methods.HttpPost;
 import cz.msebera.android.httpclient.entity.StringEntity;
+import cz.msebera.android.httpclient.impl.client.BasicResponseHandler;
 import cz.msebera.android.httpclient.impl.client.DefaultHttpClient;
 import studio.dayvidson.curriculoti.controller.Usuario;
 
@@ -19,6 +24,7 @@ import static android.os.Looper.getMainLooper;
 
 public class UsuarioWS {
     private static final String URL_INSERT = "http://192.168.15.69:8080/CurriculoWS/curriculoWS/usuario/postInsert";
+    private static final String URL_LOGIN = "http://192.168.15.69:8080/CurriculoWS/curriculoWS/usuario/login";
 
     public String insertUsuario(Usuario usuario) {
         HttpClient httpClient = new DefaultHttpClient();
@@ -46,6 +52,28 @@ public class UsuarioWS {
             e.printStackTrace();
             return e.getMessage();
         }
+    }
+
+    public String selectLogin(String usuario, String senha){
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpGet httpGet = new HttpGet(URL_LOGIN+"?user="+usuario+"&senha="+senha);
+
+        StrictMode.ThreadPolicy policy = new
+                StrictMode.ThreadPolicy.Builder().permitAll().build();
+
+        StrictMode.setThreadPolicy(policy);
+        //Processa respostas vindas do servidor e a transforma em determinado tipo
+        ResponseHandler<String> responseHandler = new BasicResponseHandler();
+
+        //executa a chamada
+        String respostaServidor = null;
+        try {
+            respostaServidor = httpClient.execute(httpGet, responseHandler);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return respostaServidor;
     }
 }
 
